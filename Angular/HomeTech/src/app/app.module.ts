bdef5992ba81524b1f7f8e277d4e863384b094ed
Koduri Bhuvan <bhuvankoduri4800@gmail.com>
2023-09-20 15:56:14 +0530

    added registration form

--- Angular/HomeTech/src/app/app.module.ts
+++ Angular/HomeTech/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,6 +9,11 @@ import { NavbarComponent } from './Components/navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+
 import { RegisterComponent } from './Components/Auth/register/register.component';
 import { LoginComponent } from './Components/Auth/login/login.component';
 
@@ -23,9 +28,14 @@ import { LoginComponent } from './Components/Auth/login/login.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
