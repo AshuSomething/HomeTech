12fe1491d44f909cf6ca89766d6464387d37cb94
Koduri Bhuvan <bhuvankoduri4800@gmail.com>
2023-09-27 13:24:29 +0530

    services and categories updated

---
+++ ASPDOTNET_Microservices/HomeTech/Services/HomeTech.Services.AcceptedRequestApi/Stripe/CouponCreateOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Stripe
+{
+	internal class CouponCreateOptions
+	{
+		public long AmountOff { get; set; }
+		public object Name { get; set; }
+		public string Currency { get; set; }
+		public object Id { get; set; }
+	}
+}
